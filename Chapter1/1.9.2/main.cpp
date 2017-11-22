eeacce30f6b4ab1e2f11f968b8cb1360679d71bc
hym_dn <yameng_he@aliyun.com>
2017-11-22 21:59:57 +0800

    提交1.9.2源码

---
+++ Chapter1/1.9.2/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+template<typename T>
+class print{
+public:
+    void operator()(const T& elem){
+        cout<<elem<<' ';
+    }
+};
+
+int main(){
+    int ia[6]={0,1,2,3,4,5};
+    vector<int> iv(ia,ia+6);
+    for_each(iv.begin(),iv.end(),print<int>());
+}
+
