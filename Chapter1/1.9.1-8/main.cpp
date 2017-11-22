70687bfedb160dfe35a6f1e3d96e1e2d4008c5a6
hym_dn <yameng_he@aliyun.com>
2017-11-22 21:36:34 +0800

    提交1.9.1-8源码

---
+++ Chapter1/1.9.1-8/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+#define __STL_TEMPLATE_NULL template<>
+
+template<class Key> struct hash{
+    void operator()(){cout<<"hash<T>"<<endl;}
+};
+
+__STL_TEMPLATE_NULL struct hash<char>{
+    void operator()(){cout<<"hash<char>*"<<endl;}
+};
+
+__STL_TEMPLATE_NULL struct hash<unsigned char>{
+    void operator()(){cout<<"hash<unsigned char>"<<endl;}
+};
+
+int main(){
+    hash<long> t1;
+    hash<char> t2;
+    hash<unsigned char> t3;
+
+    t1();
+    t2();
+    t3();
+}
