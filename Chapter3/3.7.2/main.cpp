75716b2d05977fe689b36f11f76df40425276634
hym_dn <yameng_he@aliyun.com>
2018-01-09 22:46:28 +0800

    更新3.7.2

--- Chapter3/3.7.2/main.cpp
+++ Chapter3/3.7.2/main.cpp
@@ -98,3 +98,21 @@ __STL_TEMPLATE_NULL struct __type_traits<double>{
     typedef __true_type has_trivial_destructor;
     typedef __true_type is_POD_type;
 };
+
+__STL_TEMPLATE_NULL struct __type_traits<long double>{
+    typedef __true_type has_trivial_default_constructor;
+    typedef __true_type has_trivial_copy_constructor;
+    typedef __true_type has_trivial_assignment_operator;
+    typedef __true_type has_trivial_destructor;
+    typedef __true_type is_POD_type;
+};
+
+
+template<class T>
+struct __type_traits<T*>{
+    typedef __true_type has_trivial_default_constructor;
+    typedef __true_type has_trivial_copy_constructor;
+    typedef __true_type has_trivial_assignment_operator;
+    typedef __true_type has_trivial_destructor;
+    typedef __true_type is_POD_type;
+};
