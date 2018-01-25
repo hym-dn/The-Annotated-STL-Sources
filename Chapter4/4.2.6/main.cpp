c4b50379a1d359218781146307ad9109e5079e76
hym_dn <yameng_he@aliyun.com>
2018-01-25 22:37:12 +0800

    提交源码4.2.6

--- Chapter4/4.2.6/main.cpp
+++ Chapter4/4.2.6/main.cpp
@@ -37,3 +37,65 @@ void clear(){
     erase(begin(),end()); // erase()就定义在上面
 } 
 
+//从position开始，插入n个元素，元素初值为x
+template<class T,class Alloc>
+void vector<T,Alloc>::insert(iterator position,size_type n,const T &x){
+    if(n!=0){//当n!=0才进行以下所有操作
+        if(size_type(end_of_storage-finish)>=n){
+            //备用空间大于等于“新增元素个数”
+            T x_copy=x;
+            //以下计算插入点之后的现有元素个数
+            const size_type elems_after=finish-position;
+            iterator old_finish=finish;
+            if(elems_after>n){
+                //"插入点之后的现有元素个数"大于“新增元素个数”
+                uninitialized_copy(finish-n,finish,finish);//将finish-n挪动到finish
+                finish+=n;//将vector尾端标记后移
+                copy_backward(position,old_finish-n,old_finish);//将position拷贝到old_finish
+                fill(position,position+n,x_copy);//从插入点开始填入新值
+            }else{
+                //"插入点之后的现有元素个数"小于等于“新增元素个数”
+                uninitialized_fill_n(finish,n-elems_after,x_copy);//在尾端插入新元素
+                //更新尾端
+                finish+=n-elems_after;
+                //将position挪到finish
+                uninitialized_copy(position,old_finish,finish);
+                //更新尾端
+                finish+=elems_after;
+                //填充新值
+                fill(position,old_finish,x_copy);
+            }
+        }else{
+            //备用空间小于“新增元素个数”（那就必须配置额外的内存）
+            //首先决定新长度：旧长度的两倍，或旧长度+新元素个数
+            const size_type old_size=size();
+            const size_type len=old_size+max(old_size,n);
+            //以下配置新的vector空间
+            iterator new_start=data_allocator::allocate(len);
+            iterator new_finish=new_start;
+            __STL_TRY{
+                //以下首先将旧vector的插入点之前的元素复制到新空间
+                new_finish=uninitialized_copy(start,position,new_start);
+                //以下再将新增元素（初值皆为n）填入新空间
+                new_finish=uninitialized_fill_n(new_finish,n,x);
+                //以下再将旧vector的插入点之后的元素复制到新空间
+                new_finish=uninitialized_copy(position,finish,new_finish);
+            }
+            #ifdef __STL_USE_EXCEPTIONS
+            catch(...){
+                //如有异常发生，实现“commit or rollback” semantics
+                destroy(new_start,new_finish);
+                data_allocator::deallocate(new_start,len);
+                throw;
+            }
+            #endif /* __STL_USE_EXCEPTIONS */
+            // 以下清除并释放旧的vector
+            destroy(start,finish);
+            deallocate();
+            //以下调整水位标记
+            start=new_start;
+            finish=new_finish;
+            end_of_storage=new_start+len;
+        }
+    }
+}
