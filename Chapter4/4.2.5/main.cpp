a89e554bfde27f23044f4448b8407f9285524d03
hym_dn <yameng_he@aliyun.com>
2018-01-25 17:00:49 +0800

    提交源码4.2.5

---
+++ Chapter4/4.2.5/main.cpp
@@ -0,0 +1,73 @@
+//finename : 4vector-test.cpp
+#include<vector>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    
+    int i;
+    vector<int> iv(2,9);
+    cout<<"size="<<iv.size()<<endl; //size=2
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=2
+
+    iv.push_back(1);
+    cout<<"size="<<iv.size()<<endl; //size=3
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=4
+
+    iv.push_back(2);
+    cout<<"size="<<iv.size()<<endl; //size=4
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=4
+
+    iv.push_back(3);
+    cout<<"size="<<iv.size()<<endl; //size=5
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+
+    iv.push_back(4);
+    cout<<"size="<<iv.size()<<endl; //size=6
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+
+    for(i=0;i<iv.size();++i) //9 9 1 2 3 4
+        cout<<iv[i]<<' ';
+    cout<<endl;
+
+    iv.push_back(5);
+
+    cout<<"size="<<iv.size()<<endl; //size=7
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+    for(i=0;i<iv.size();++i) //9 9 1 2 3 4 5
+        cout<<iv[i]<<' '; 
+    cout<<endl;
+
+    iv.pop_back();
+    iv.pop_back();
+    cout<<"size="<<iv.size()<<endl; //size=5
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+
+    iv.pop_back();
+    cout<<"size="<<iv.size()<<endl; //size=4
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+
+    vector<int>::iterator ivite=find(iv.begin(),iv.end(),1);
+    if(ivite!=iv.end()) iv.erase(ivite);
+    cout<<"size="<<iv.size()<<endl; //size=3
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+    for(i=0;i<iv.size();++i)
+        cout<<iv[i]<<' '; //9 9 2
+    cout<<endl;
+
+    ivite=find(ivec.begin(),iv.end(),2);
+    if(ivite!=iv.end())iv.insert(ivite,3,7);
+
+    cout<<"size="<<iv.size()<<endl; //size=6
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+    for(int i=0;i<ivec.size();++i) //9 9 7 7 7 2
+        cout<<ivec[i]<<' ';
+    cout<<endl;
+
+    iv.clear();
+    cout<<"size="<<iv.size()<<endl; //size=0
+    cout<<"capacity="<<iv.capacity()<<endl; //capacity=8
+
+}
